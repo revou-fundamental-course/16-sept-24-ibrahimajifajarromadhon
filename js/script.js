6c7a69a887e77734a8697e23d8fe44b004d7d2c1
unknown <ajii4262@gmail.com>
2024-09-24 22:46:45 +0700

    slider+responsive

--- js/script.js
+++ js/script.js
@@ -29,3 +29,43 @@ function displayInput() {
 
     return false;
 }
+
+let currentSlideIndex = 0;
+const slides = [
+    'https://picsum.photos/1920/1080',
+    'https://picsum.photos/1920/1080?random=1',
+    'https://picsum.photos/1920/1080?random=2'
+];
+
+// Function to change slide
+function changeSlide(direction) {
+    currentSlideIndex += direction;
+
+    // Wrap around if out of bounds
+    if (currentSlideIndex < 0) {
+        currentSlideIndex = slides.length - 1;
+    } else if (currentSlideIndex >= slides.length) {
+        currentSlideIndex = 0;
+    }
+
+    updateBackground();
+}
+
+function updateBackground() {
+    document.getElementById("my-profile").style.backgroundImage = `url('${slides[currentSlideIndex]}')`;
+    updateIndicators();
+}
+
+function updateIndicators() {
+    const indicators = document.querySelectorAll('.indicator');
+    indicators.forEach((indicator, index) => {
+        indicator.classList.toggle('active', index === currentSlideIndex);
+    });
+}
+
+function currentSlide(index) {
+    currentSlideIndex = index;
+    updateBackground();
+}
+
+updateBackground();
